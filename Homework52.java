5572f8d2208dd4c5b0657df65c37844ed8cd96f4
Anatoly <107132732+Seven7897@users.noreply.github.com>
2022-11-29 19:55:27 +0300

    Create Homework52.java

---
+++ Homework52.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Homework52 {
+    public static void main(String[] args) {
+        String[] words1 = {"leetcode", "is", "amazing", "as", "is"};
+        String[] words2 = {"amazing", "leetcode", "is"};
+        System.out.println(countWords(words1, words2));
+    }
+
+    public static int countWords(String[] words1, String[] words2) {
+        if (words1.length > words2.length) {
+            return countWords(words2, words1);
+        }
+
+        Map<String, Integer> wordsMap = new HashMap<>();
+
+        for (String word : words1) {
+            wordsMap.putIfAbsent(word, 0);
+            wordsMap.put(word, wordsMap.get(word) + 1);
+        }
+
+        for (String word : words2) {
+            if (wordsMap.containsKey(word) && wordsMap.get(word) <= 1) {
+                wordsMap.put(word, wordsMap.get(word) - 1);
+            }
+        }
+
+        int wordsWithOneOccurenceCounter = 0;
+
+        for (String key : wordsMap.keySet()) {
+            if (wordsMap.get(key).equals(0)) wordsWithOneOccurenceCounter++;
+        }
+        return wordsWithOneOccurenceCounter;
+    }
+}
